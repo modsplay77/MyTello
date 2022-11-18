c175cb08b15b25067433bd10c887a07594bd9cc0
studi <modsplay90@gmail.com>
2022-11-19 00:17:21 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mytello/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.mytello;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button boton = findViewById(R.id.boton);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), inicio.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+    }
+}
