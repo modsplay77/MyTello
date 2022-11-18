23effc138be11720abc00aa77663510e4a1a8c04
studi <modsplay90@gmail.com>
2022-11-19 00:45:07 +0100

    1

--- app/src/main/java/com/example/mytello/inicio.java
+++ app/src/main/java/com/example/mytello/inicio.java
@@ -1,7 +1,10 @@
 package com.example.mytello;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class inicio extends AppCompatActivity {
 
@@ -10,6 +13,22 @@ public class inicio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicio);
 
+        Button nuevo = findViewById(R.id.proyecto);
+        nuevo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), nuevo.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+        Button cargar = findViewById(R.id.cargar);
+        cargar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), cargar.class);
+                startActivityForResult(intent, 0);
+            }
+        });
 
     }
 
