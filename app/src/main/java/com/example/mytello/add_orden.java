8b6d40ca6fea7d2daad8bc6abbcd85506a2ab4a1
Modsplay77 <modsplay90@gmail.com>
2022-11-23 13:03:35 +0100

    +3

--- app/src/main/java/com/example/mytello/add_orden.java
+++ app/src/main/java/com/example/mytello/add_orden.java
@@ -1,13 +1,12 @@
 package com.example.mytello;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 public class add_orden extends AppCompatActivity {
 
@@ -16,21 +15,36 @@ public class add_orden extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_orden);
         Button boton = findViewById(R.id.guardar);
-
+        EditText orden;
+        /*Listado de ordenes posibles
+        arriva
+        abajo
+        giro positivo
+        giro negativo
+        flip izq
+        flip der
+        adelante
+        atras
+        */
+        orden = this.<EditText>findViewById(R.id.op1);
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), add_orden.class);
-                startActivityForResult(intent, 0);
+                String op1 = orden.getText().toString();
+
+                Intent intent = new Intent(add_orden.this, mostrar_datos.class);
+
                 // añadir funcion para guardar los datos introducidos por el usuario
+                intent.putExtra("Orden", op1);
+                startActivityForResult(intent, 0);
                 Toast.makeText(add_orden.this, "Guardado con éxito", Toast.LENGTH_SHORT).show();
             }
         });
 
 
 
-
-            }
+    }
 
 
     }
+
