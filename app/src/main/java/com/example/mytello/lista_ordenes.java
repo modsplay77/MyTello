dca1f605ab71bf3502dba1464af77e97565c035e
modsplay77 <modsplay90@gmail.com>
2022-12-12 22:33:44 +0100

    +16

--- app/src/main/java/com/example/mytello/lista_ordenes.java
+++ app/src/main/java/com/example/mytello/lista_ordenes.java
@@ -1,65 +1,34 @@
 package com.example.mytello;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.provider.BaseColumns;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
-import android.widget.ListAdapter;
-
-import java.util.ArrayList;
+import android.widget.TextView;
 
 public class lista_ordenes extends AppCompatActivity {
-
-    private Button up,down,left,right,forward,back,cw,ccw,flip,speed;
+    TextView pro_actual;
+     String nombre;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lista_ordenes);
-//crear ordenes, 1era, 2da..etc.. y mostrarlos en un fragment en este layout,
-// confirmar para poder añadirlo en el layout en la activity "Nuevo" con un fragment.
-
-
-        setContentView(R.id.pro_actual);
-
-
-     /*   Button up = findViewById(R.id.guardar_orden);
-        Button down = findViewById(R.id.guardar_orden);
-        Button left = findViewById(R.id.guardar_orden);
-        Button right = findViewById(R.id.guardar_orden);
-        Button forward = findViewById(R.id.guardar_orden);
-        Button back = findViewById(R.id.guardar_orden);
-        Button cw = findViewById(R.id.guardar_orden);
-        Button ccw = findViewById(R.id.guardar_orden);
-        Button flip = findViewById(R.id.guardar_orden);
-        Button speed = findViewById(R.id.guardar_orden);
-
-*/
+        datos();
 
     }
-    public final class FeedReaderContract {
-        // To prevent someone from accidentally instantiating the contract class,
-        // make the constructor private.
-        private FeedReaderContract() {}
 
-        /* Inner class that defines the table contents */
-        public class FeedEntry implements BaseColumns {
-            public static final String TABLE_NAME = "entry";
-            public static final String COLUMN_NAME_TITLE = "title";
-            public static final String COLUMN_NAME_SUBTITLE = "subtitle";
-        }
+    private void datos() {
+        Bundle extras = getIntent().getExtras();
+
+        String datos= extras.putString(nombre);
+        pro_actual = (TextView) findViewById(R.id.proyecto);
+        pro_actual.setText(datos);
     }
-    public static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE " + FeedReaderContract.FeedEntry.TABLE_NAME + " (" +
-                    FeedReaderContract.FeedEntry._ID + " INTEGER PRIMARY KEY," +
-                    FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE + " TEXT," +
-                    FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE + " TEXT)";
 
-    private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + FeedReaderContract.FeedEntry.TABLE_NAME;
+
+
 
 
 }
--- app/src/main/java/com/example/mytello/nuevo.java
+++ app/src/main/java/com/example/mytello/nuevo.java
@@ -1,40 +1,27 @@
 package com.example.mytello;
 
-import static com.example.mytello.R.layout.fragment_conf_orden;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class nuevo extends AppCompatActivity {
 
-    private Object guardar;
-    private Object nombre;
-    private View view;
+    String nombre = "Proyecto nuevo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nuevo);
 
-
-           // guardar = view.findViewById(R.id.guardar);
-            //nombre = view.findViewById(R.id.nombre);
-
-
-        Button guardar = findViewById(R.id.guardar);;
+        Button guardar = findViewById(R.id.guardar);
             guardar.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Intent intent = new Intent(v.getContext(), lista_ordenes.class);
+                    Intent extra = intent.putExtra(nombre);
                     startActivityForResult(intent, 0);
                 }
             });
